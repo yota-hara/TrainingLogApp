f4d62c27e282ef3017eeff8ee4d345b6b6aba934
yota-hara <116154955+yota-hara@users.noreply.github.com>
2022-11-08 01:31:48 +0900

    Initial Commit

---
+++ TrainingLogApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrainingLogApp
+//
+//  Created by 田原葉 on 2022/11/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
